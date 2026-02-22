3426ced90b13297e019a5f59e398ae084e286b74
SEGlendinning <scottglendinning@kanoncare.health>
2026-02-22 11:11:26 -0500

    Lead Capture Update 2

--- amplify/resource.ts
+++ amplify/resource.ts
@@ -3,6 +3,9 @@ import { defineFunction, secret } from '@aws-amplify/backend';
 export const captureLead = defineFunction({
   name: 'capture-lead',
   environment: {
+    GOOGLE_CLIENT_EMAIL: secret('GOOGLE_CLIENT_EMAIL'),
+    GOOGLE_PRIVATE_KEY: secret('GOOGLE_PRIVATE_KEY'),
+    GOOGLE_PROJECT_ID: secret('GOOGLE_PROJECT_ID'),
     SPREADSHEET_ID: secret('SPREADSHEET_ID'),
   },
 });
